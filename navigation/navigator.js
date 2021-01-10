3b8103cc82a4bbb95403c7a478cafa50f9f4e276
Rubai Bian <lnbyk@udel.edu>
2021-01-09 21:12:17 -0500

    init project

---
+++ navigation/navigator.js
@@ -0,0 +1,8 @@
+import { createStackNavigator } from "react-navigation-stack";
+import { createAppContainer } from "react-navigation";
+
+const MealsNavigator = createStackNavigator({
+
+});
+
+export default createAppContainer(MealsNavigator);
