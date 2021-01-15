7e1aa82e8f1bc11bbd67d9d948a0d75ce1bcdcea
kimjin <jinente@udel.edu>
2021-01-15 12:39:15 -0500

    json

---
+++ screens/StartScreen.js
@@ -0,0 +1,16 @@
+import React from "react";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ImageBackground,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from "react-native";
+
+const StartScreen = (props) => {
+    
+}
+
+export default StartScreen;
